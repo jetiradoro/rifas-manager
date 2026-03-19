16fe8c656fa65e45b79082754ffef8a9d5a824fd
Jesús.T <jesus@jetiradoro.com>
2026-03-19 23:57:28 +0100

    test

--- rifas-api/dokploy/entrypoint.sh
+++ rifas-api/dokploy/entrypoint.sh
@@ -3,16 +3,16 @@ set -e
 
 cd /app
 
-# tail -f /dev/null & wait
+tail -f /dev/null & wait
 
-export PRISMA_ENGINES_CACHE_DIR=/tmp/prisma
-mkdir -p "$PRISMA_ENGINES_CACHE_DIR"
+# export PRISMA_ENGINES_CACHE_DIR=/tmp/prisma
+# mkdir -p "$PRISMA_ENGINES_CACHE_DIR"
 
-if [ "$NODE_ENV" = "production" ]; then
-  npx prisma migrate deploy
-  exec npm run start:prod
-else
-  npx prisma migrate dev
-  npx prisma generate
-  exec npm run start:dev
-fi
+# if [ "$NODE_ENV" = "production" ]; then
+#   npx prisma migrate deploy
+#   exec npm run start:prod
+# else
+#   npx prisma migrate dev
+#   npx prisma generate
+#   exec npm run start:dev
+# fi
