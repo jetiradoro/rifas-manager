13623311bc281a9905365dcc76c30561f5b47357
Jesús.T <jesus@jetiradoro.com>
2026-03-21 21:09:58 +0100

    change entry point

--- rifas-app/dokploy/entrypoint.sh
+++ rifas-app/dokploy/entrypoint.sh
@@ -4,9 +4,8 @@ set -e
 cd /app
 
 if [ "${NODE_ENV}" = "production" ]; then
-  npm run build
   # Servir archivos estáticos en producción (necesitarás un servidor como serve)
-  npx serve -s dist/pwa -l 80
+  npx serve -s dist/pwa -l 3000
 else
   npm run dev
 fi
